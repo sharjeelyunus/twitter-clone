f8fa96eebbef31f3b2a87e811a88a224eeef4c02
sharjeelyunus <sharjeel924@gmail.com>
2021-06-09 00:45:05 +0500

    📦 NEW: Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Twitter Clone🚀</h1>
+      <Sidebar />
     </div>
   );
 }
