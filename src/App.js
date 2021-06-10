ad0f3e3e771ad497994fab236866f8bd2ff3ac85
sharjeelyunus <sharjeel924@gmail.com>
2021-06-10 08:49:19 +0500

    📦 NEW: Feed - Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Sidebar from './Sidebar';
+import Feed from './Feed';
+import Widgets from './Widgets';
 
 function App() {
   return (
     <div className="app">
-      <h1>Twitter Clone🚀</h1>
       <Sidebar />
+      <Feed />
+      <Widgets />
     </div>
   );
 }
---
+++ src/Feed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Feed.css';
+
+function Feed() {
+    return (
+        <div className="feed">
+            <div className="feed__header">
+                <h2>Home</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Feed;
