7d7a9644642b4529c6b90d5b50c0cd390e02fa9c
sharjeelyunus <sharjeel924@gmail.com>
2021-06-10 06:32:35 +0500

    👌 IMPROVE: SideBar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -15,7 +15,7 @@ import { Button } from '@material-ui/core';
 function Sidebar() {
     return (
         <div className="sidebar">
-            <TwitterIcon />
+            <TwitterIcon className="sidebar__twitterIcon" />
 
             <SidebarOption active Icon={HomeIcon} text="Home" />
             <SidebarOption Icon={SearchIcon} text="Explore" />
